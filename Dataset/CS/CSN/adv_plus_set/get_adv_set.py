5a9ca5e9afbcfb5804c54b87f140e279b8755a87
Alin <126737088+ALinrunrun@users.noreply.github.com>
2026-03-26 05:58:13 +1000

    Update get_adv_set.py

--- Dataset/CS/CSN/adv_plus_set/get_adv_set.py
+++ Dataset/CS/CSN/adv_plus_set/get_adv_set.py
@@ -1,7 +1,7 @@
 import os
 import sys
-sys.path.append('../../../../')
-sys.path.append('../../../../python_parser')
+sys.path.append('../../../')
+sys.path.append('../../../python_parser')
 import json
 import pandas as pd
 import shutil
@@ -11,7 +11,7 @@ def get_adv_set(csv_path, model_name):
 
     index_to_target = {}
 
-    with open("../test_sampled.jsonl", "r", encoding="utf-8") as f:
+    with open("../CSN/test_sampled.jsonl", "r", encoding="utf-8") as f:
         for i, line in enumerate(f):
             if i >= 1000:
                 break
@@ -23,17 +23,15 @@ def get_adv_set(csv_path, model_name):
 
     output_dir = os.path.dirname(csv_path)
     base_name = os.path.splitext(os.path.basename(csv_path))[0]
-    adv_set_output_file = os.path.join(output_dir, f"adv_by_{base_name}.jsonl")
+    adv_set_output_file = os.path.join(output_dir, f"test_data_{model_name}_{base_name}.jsonl")
 
 
     df = pd.read_csv(csv_path)
 
     df = df[["Index", "Original Code", "Adversarial Code", "Type"]]
 
-    if "strike" in csv_path.lower():
-        df = df[(df["Index"] >= 500) & (df["Index"] < 1000)]
-    else:
-        df = df[df["Index"] < 1000]
+    df = df[(df["Index"] >= 500) & (df["Index"] < 1000)]
+    # df = df[df["Index"] < 1000]
 
     success_rows = df["Original Code"].notna() & (df["Original Code"].str.strip() != "")
     success_count = success_rows.sum()
@@ -43,7 +41,7 @@ def get_adv_set(csv_path, model_name):
     for Index, adv in zip(df.loc[success_rows, "Index"], df.loc[success_rows, "Adversarial Code"]):
         new_code_tokens = get_code_tokens_dataset(adv, "java")   
         new_entry = {
-            "original_string": "strike",
+            "original_string": base_name,
             "language": "java",
             "code": adv,
             "code_tokens": new_code_tokens,
